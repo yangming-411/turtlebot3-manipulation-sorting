999ec2d6e146ed9a328e56b37a5cbff7070c601d
yangming-411 <2023177024@qq.com>
2025-11-26 15:09:50 +0800

    初始化项目结构：创建基础视觉感知目录和空文件

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# turtlebot3-manipulation-sorting
-Description: ROS2-based TurtleBot3 with manipulator for object sorting in 6-room environment
+# TurtleBot3机械臂分拣项目
+
+## 项目描述
+基于ROS2 Humble的TurtleBot3机械臂分拣系统
+
+## 安装说明
+1. 克隆仓库:git clone https://github.com/yangming-411/turtlebot3-manipulation-sorting.git
+2. 运行依赖安装脚本：`./install_dependencies.sh`
+3. 将项目放入工作空间src目录
+4. 编译：`colcon build`
+5. 运行：`source install/setup.bash`
+
---
+++ turtlebot3_manipulation_project/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'turtlebot3_manipulation_project'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),\
+        (os.path.join('share', package_name, 'config/perception'), glob('config/perception/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='xiuyan',
+    maintainer_email='xiuyan@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        
+        ],
+    },
+)
